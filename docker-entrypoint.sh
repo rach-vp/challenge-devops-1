e7776005c89ec5bf44fd096ecd2a445f3935f34f
Raquel Picanço <picanco.raquel@gmail.com>
2022-06-27 16:40:01 -0400

    feat: Set up containers settings files

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+python manage.py migrate
+
+# Start server
+python manage.py runserver 0.0.0.0:8000
